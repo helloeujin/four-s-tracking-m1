e8e55ed972996a98209ed2c5839271642f53820c
Youjin <meggonagul@gmail.com>
2022-11-24 17:20:35 +0900

    remove Stack navigation on Drawer item list

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import Profile from "../screens/Profile";
 
 const DrawerNav = createDrawerNavigator();
+
+const CustomDrawerContent = (props) => {
+  const { state, ...rest } = props;
+  const newState = { ...state };
+  newState.routes = newState.routes.filter((item) => item.name !== "Stack");
+
+  // console.log(newState.ourtes);
+
+  return (
+    <DrawerContentScrollView {...props}>
+      {/* <DrawerItemList {...props} /> */}
+      <DrawerItemList state={newState} {...rest} />
+    </DrawerContentScrollView>
+  );
+};
 
 const Drawer = () => {
   return (
     <DrawerNav.Navigator
       initialRouteName="Profile"
       useLegacyImplementation={true}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
       <DrawerNav.Screen
         name="Profile"
--- navigation/Root.js
+++ navigation/Root.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import Tabs from "./Tabs";
 import Stack from "./Stack";
-import Drawer from "./Drawer";
+// import Drawer from "./Drawer";
 import Home from "../screens/Home";
 
 // const Nav = createNativeStackNavigator();
 const Nav = createDrawerNavigator();
 
+const CustomDrawerContent = (props) => {
+  const { state, ...rest } = props;
+  const newState = { ...state };
+  newState.routes = newState.routes.filter((item) => item.name !== "Stack");
+
+  // FILTER OUT STACK NAV ON DRAWER ITEM
+  return (
+    <DrawerContentScrollView {...props}>
+      {/* <DrawerItemList {...props} /> */}
+      <DrawerItemList state={newState} {...rest} />
+    </DrawerContentScrollView>
+  );
+};
+
 const Root = () => (
   <Nav.Navigator
     // initialRouteName="Profile"
     useLegacyImplementation={true}
+    drawerContent={(props) => <CustomDrawerContent {...props} />}
     screenOptions={{
       headerShown: false,
     }}
