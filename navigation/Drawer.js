669baf5945d04ece986988708d35cc94594c3e3a
Youjin <meggonagul@gmail.com>
2022-12-13 21:58:15 +0900

    custom drawer with profile img

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -2,18 +2,35 @@ import React from "react";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
+  DrawerItem,
   DrawerItemList,
 } from "@react-navigation/drawer";
 import Profile from "../screens/Profile";
 import Tabs from "./Tabs";
+import { Text } from "react-native";
+import { Feather, Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 const DrawerNav = createDrawerNavigator();
+
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem
+        label=""
+        onPress={() => alert("Link to help")}
+        icon={({}) => <MaterialIcons name="face" size={83} color="#999" />}
+      />
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
 
 const Drawer = () => {
   return (
     <DrawerNav.Navigator
       initialRouteName="Profile"
       useLegacyImplementation={true}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
         headerShown: false,
       }}
